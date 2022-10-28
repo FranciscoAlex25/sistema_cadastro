28b91f6fcb331f10c230476470f2e554facfc7e6
Francisco Alex <franciscoalex.bio@gmail.com>
2022-10-28 20:59:52 -0300

    criando o excluir e atualizar

--- sistema_cadastro.py
+++ sistema_cadastro.py
@@ -14,7 +14,8 @@ def mostrarTela():
 
     # menu
     print(67 * '-')
-    print('{cad: ^22} {alt: ^22} {apa: ^22}'.format(cad=cadastrar, alt=alterar, apa=apagar))
+    print('{cad: ^22} {alt: ^22} {apa: ^22}'.format(cad=cadastrar,
+                                                    alt=alterar, apa=apagar))
     print(67 * '-')
     
     # tabela dos produtos
@@ -44,6 +45,10 @@ def mostrarTela():
     if escolha.isnumeric():
         if escolha == '1':
             cadastrarProd()
+        elif escolha == '3':
+            excluirProd()
+        elif escolha == '2':
+            atualizarProd()
 
     # recomeçando
     mostrarTela()
@@ -56,5 +61,44 @@ def cadastrarProd():
     preco = input('Preço do produto: ')
     lista_produto.append([cod, produto, preco])
 
+# função para excluir produto
+def excluirProd():
+    cod = int(input('Informe o ID do produto para excluir: '))
+    for i in lista_produto:
+        if i[0] == cod:
+            lista_produto.remove(i)
+            break
 
+# função para atular dados
+def atualizarProd():
+    global lista_produto
+    cont = 0
+    
+    cod = input('Qual o código do produto que deseja atualizar? ')
+    valor = input('O que deseja atulizar, nome (1) ou preço (2)? ')
+
+    if cod.isnumeric():
+        if valor.isnumeric():
+            if valor == '1':
+                dado = input('digite o novo nome: ')
+                for i in lista_produto:
+                    for j in i:
+                        if str(j) == cod:
+                            lista_produto[cont][1] = dado
+                            break
+                    cont += 1
+            else:
+                dado = input('digite o novo preço: ')
+                for i in lista_produto:
+                    for j in i:
+                        if str(j) == cod:
+                            lista_produto[cont][2] = dado
+                            break
+                    cont += 1
+        else:
+            print('Opção inválida!')
+    else:
+        print('Código inválido!')
+
+        
 mostrarTela()
